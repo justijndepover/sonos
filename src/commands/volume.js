597955b6e990d70250a13894a7b06d4ddc626a14
Justijn Depover <justijndepover@gmail.com>
2021-01-17 13:56:13 +0100

    validate volume

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -3,8 +3,15 @@ exports.description = 'Set the volume';
 exports.callback = function (yargs, device) {
     var volume = yargs.argv._[1];
     if (! volume) {
+        console.log('Volume is required');
         process.exit();
     }
-    // validate
+
+    volume = parseInt(volume);
+    if (volume < 0 || volume > 100) {
+        console.log('Volume should be between 0 and 100');
+        process.exit();
+    }
+
     device.setVolume(volume).then(() => console.log(`Setting volume to ${volume}`));
 }
